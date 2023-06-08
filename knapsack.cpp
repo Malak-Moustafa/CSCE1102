0b109ccd0a3b76550b9caaa801904e11b19cb25c
Sama-Amr <samaamr@aucegypt.edu>
2023-06-08 14:34:59 +0300

    commit2

--- knapsack.cpp
+++ knapsack.cpp
@@ -19,12 +19,3 @@ return max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt,
 val, n - 1));
 }
 
-int main()
-{
-int val[] = { 60, 100, 120 };
-int wt[] = { 10, 20, 30 };
-int W = 50;
-int n = sizeof(val) / sizeof(val[0]);
-cout << knapSack(W, wt, val, n);
-return 0;
-}
